f76206bff8be92ecbc88791944e54d9424855f8f
OkODevelopment <balthazar.beguin.perso@gmail.com>
2025-01-09 16:21:00 +0100

    Patch API Request Number

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -22,7 +22,7 @@ export default function Home() {
     const fetchCryptos = async () => {
       try {
         const response = await fetch(
-          'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+            'https://cors-anywhere.herokuapp.com/https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
         );
         if (!response.ok) {
           throw new Error('Échec de récupération des données');
